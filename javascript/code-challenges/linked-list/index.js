d96fe257808fbd6bc406f586424da03d7b9fb581
Motasem-Sulaiman <motasemismael0@gmail.com>
2023-07-02 15:25:35 +0000

    challenge 10

--- javascript/code-challenges/linked-list/index.js
+++ javascript/code-challenges/linked-list/index.js
@@ -132,4 +132,54 @@ class newLinkedList {
   }
 }
 
-module.exports = {linkedList,newLinkedList};
+/////////////////////////////////////////////challenge 10
+class Stack {
+  constructor() {
+    this.top = null;
+    this.length = 0;
+  }
+  isEmpty() {
+    if (this.top === null) {
+      return true;
+    } else {
+      return false;
+    }
+
+
+  }
+
+  push(value) {
+    if (this.isEmpty()) {
+      const newNode = new Node(value);
+      this.top = newNode;
+      this.length++;
+    } else {
+      const newNode = new Node(value);
+      newNode.next = this.top;
+      this.top = newNode;
+      this.length++;
+    }
+  }
+  pop() {
+    if (this.isEmpty()) {
+      console.log("empty stack");
+      return false;
+    }
+    const temp = this.top;
+    this.top = this.top.next;
+    temp.next = null;
+    this.length--;
+    return temp.value;
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return "sorry stack is empty";
+    }
+    return this.top.value;
+  }
+}
+
+
+
+module.exports = { linkedList, newLinkedList,Stack };
